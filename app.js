1030e900ed311a038cb33d26d70a8bbaf274070d
keval vaja <120974030+Kevalvaja@users.noreply.github.com>
2024-09-17 12:40:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req,res) => {
+    console.log("call vercel commit");
+})
+
 const swaggerDefinition = {
     openapi: '3.0.0',
     info: {
